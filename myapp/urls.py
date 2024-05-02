b20a378a90419720e334961b1a241e6c5c2a7079
Albert <131027712+AlbertoGramberg@users.noreply.github.com>
2024-05-02 08:43:42 -0300

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,21 +4,12 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from myapp.views import generate_wordcloud
 
 urlpatterns = [
-    path('', views.inicio),
-    path('about/', views.about),
-    path('novedades/', views.Novedades),
-    path('projects/<int:id>', views.project),
-    path('noticias/<int:id>', views.Noticias),
-    path('video/', views.lista_videos),
-    path('palabras/', views.generate_wordcloud, name='wordcloud'),
-    path('videos/<str:palabra_busqueda>/', views.lista_videos, name='lista_videos'),
+    path('', views.personal),
     path('web/', views.personal, name='personal'),
     path('blog/', views.post_view, name=''),
     path('blog/<int:id>', views.blogs, name=''),
-    path('articulo/<int:id>', views.article_detail, name='render_items'),
     path('formulario/', views.crear_post, name='crear_post'),
 
 
