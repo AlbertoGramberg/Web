d85bc9808d428192c3430b9c1894351a654f0ee5
Albert <131027712+AlbertoGramberg@users.noreply.github.com>
2024-05-02 08:53:42 -0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -34,7 +34,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myapp',
-    'content_editor',
     'django_quill',
 
 ]
